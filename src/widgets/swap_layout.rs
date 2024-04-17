620786260b79c086ac06a0a17086499ace9d94f5
Daniel Jankowski <daniel.jankowski@rub.de>
2024-04-17 13:50:00 +0200

    feat: add formats for swap layout

--- src/widgets/swap_layout.rs
+++ src/widgets/swap_layout.rs
@@ -1,13 +1,30 @@
 use std::collections::BTreeMap;
 use zellij_tile::shim::next_swap_layout;
 
+use crate::render::FormattedPart;
 use crate::{config::ZellijState, widgets::widget::Widget};
 
-pub struct SwapLayoutWidget {}
+pub struct SwapLayoutWidget {
+    format: Vec<FormattedPart>,
+    hide_if_empty: bool,
+}
 
 impl SwapLayoutWidget {
-    pub fn new(_config: &BTreeMap<String, String>) -> Self {
-        Self {}
+    pub fn new(config: &BTreeMap<String, String>) -> Self {
+        let mut format: Vec<FormattedPart> = Vec::new();
+        if let Some(form) = config.get("swap_layout_format") {
+            format = FormattedPart::multiple_from_format_string(form);
+        }
+
+        let hide_if_empty = match config.get("swap_layout_hide_if_empty") {
+            Some(hide_if_empty) => hide_if_empty == "true",
+            None => false,
+        };
+
+        Self {
+            format,
+            hide_if_empty,
+        }
     }
 }
 
@@ -21,10 +38,32 @@ impl Widget for SwapLayoutWidget {
 
         let active_tab = active_tab.unwrap();
 
-        match active_tab.active_swap_layout_name.clone() {
+        let name = match active_tab.active_swap_layout_name.clone() {
             Some(n) => n,
             None => "".to_owned(),
+        };
+
+        if name.is_empty() && self.hide_if_empty {
+            return "".to_owned();
         }
+
+        if self.format.is_empty() {
+            return name;
+        }
+
+        let mut output = "".to_owned();
+
+        for f in &self.format {
+            let mut content = f.content.clone();
+
+            if content.contains("{name}") {
+                content = content.replace("{name}", &name);
+            }
+
+            output = format!("{}{}", output, f.format_string(&content));
+        }
+
+        output
     }
 
     fn process_click(&self, _state: &ZellijState, _pos: usize) {

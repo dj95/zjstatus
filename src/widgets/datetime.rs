5dea90349c6366383ecda9a1b9bf3e321824a5c6
Daniel Jankowski <daniel.jankowski@rub.de>
2024-06-20 20:37:22 +0200

    feat: enable configuration for date and time separately
    fixes #70

--- src/widgets/datetime.rs
+++ src/widgets/datetime.rs
@@ -9,7 +9,9 @@ use crate::{config::ZellijState, widgets::widget::Widget};
 
 pub struct DateTimeWidget {
     format: String,
-    color_format: FormattedPart,
+    time_format: String,
+    date_format: String,
+    color_format: Vec<FormattedPart>,
     time_zone: Option<Tz>,
 }
 
@@ -18,6 +20,16 @@ impl DateTimeWidget {
         let mut format = "%H:%M";
         if let Some(form) = config.get("datetime_format") {
             format = form;
+        }
+
+        let mut time_format = "%H:%M";
+        if let Some(form) = config.get("datetime_time_format") {
+            time_format = form;
+        }
+
+        let mut date_format = "%Y-%m-%d";
+        if let Some(form) = config.get("datetime_date_format") {
+            date_format = form;
         }
 
         let mut time_zone_string = "Etc/UTC";
@@ -37,30 +49,63 @@ impl DateTimeWidget {
 
         Self {
             format: format.to_owned(),
-            color_format: FormattedPart::from_format_string(color_format),
+            date_format: date_format.to_owned(),
+            time_format: time_format.to_owned(),
+            color_format: FormattedPart::multiple_from_format_string(color_format),
             time_zone,
         }
     }
 }
 
 impl Widget for DateTimeWidget {
-    fn process(&self, _name: &str ,_state: &ZellijState) -> String {
-        let mut output = self.color_format.content.clone();
-        if output.contains("{format}") {
-            let date = Local::now();
+    fn process(&self, _name: &str, _state: &ZellijState) -> String {
+        let date = Local::now();
 
-            let mut tz = Tz::UTC;
-            if let Some(t) = self.time_zone {
-                tz = t;
-            }
-
-            output = output.replace(
-                "{format}",
-                format!("{}", date.with_timezone(&tz).format(self.format.as_str())).as_str(),
-            );
+        let mut tz = Tz::UTC;
+        if let Some(t) = self.time_zone {
+            tz = t;
         }
 
-        self.color_format.format_string(&output)
+        self.color_format
+            .iter()
+            .map(|f| {
+                let mut content = f.content.clone();
+
+                if content.contains("{format}") {
+                    content = content.replace(
+                        "{format}",
+                        format!("{}", date.with_timezone(&tz).format(self.format.as_str()))
+                            .as_str(),
+                    );
+                }
+
+                if content.contains("{date}") {
+                    content = content.replace(
+                        "{date}",
+                        format!(
+                            "{}",
+                            date.with_timezone(&tz).format(self.date_format.as_str())
+                        )
+                        .as_str(),
+                    );
+                }
+
+                if content.contains("{time}") {
+                    content = content.replace(
+                        "{time}",
+                        format!(
+                            "{}",
+                            date.with_timezone(&tz).format(self.time_format.as_str())
+                        )
+                        .as_str(),
+                    );
+                }
+
+                (f, content)
+            })
+            .fold("".to_owned(), |acc, (f, content)| {
+                format!("{acc}{}", f.format_string(&content))
+            })
     }
 
     fn process_click(&self, _name: &str, _state: &ZellijState, _pos: usize) {}

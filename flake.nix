2bb466d6b2f1c53771287982b5d61036e553619c
Daniel Jankowski <daniel.jankowski@rub.de>
2024-12-20 17:48:26 +0100

    fix: fix crane nix dep to older commit

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     crane = {
-      url = "github:ipetkov/crane";
+      url = "github:ipetkov/crane/af1556ecda8bcf305820f68ec2f9d77b41d9cc80";
     };
 
     flake-utils.url = "github:numtide/flake-utils";

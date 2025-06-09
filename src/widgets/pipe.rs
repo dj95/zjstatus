2ecc0842c7e3ad41115613857346af5e2083e329
Daniel Jankowski <daniel.jankowski@rub.de>
2025-06-09 13:56:34 +0200

    feat(pipe): add render modes

--- src/widgets/pipe.rs
+++ src/widgets/pipe.rs
@@ -2,7 +2,7 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::BTreeMap;
 
-use crate::render::FormattedPart;
+use crate::render::{FormattedPart, formatted_parts_from_string_cached};
 
 use super::widget::Widget;
 
@@ -10,19 +10,29 @@ lazy_static! {
     static ref PIPE_REGEX: Regex = Regex::new("_[a-zA-Z0-9]+$").unwrap();
 }
 
+#[derive(Clone, Debug, PartialEq)]
+enum RenderMode {
+    Static,
+    Dynamic,
+    Raw,
+}
+
 pub struct PipeWidget {
     config: BTreeMap<String, PipeConfig>,
+    zj_conf: BTreeMap<String, String>,
 }
 
 #[derive(Clone)]
 struct PipeConfig {
     format: Vec<FormattedPart>,
+    render_mode: RenderMode,
 }
 
 impl PipeWidget {
     pub fn new(config: &BTreeMap<String, String>) -> Self {
         Self {
             config: parse_config(config),
+            zj_conf: config.clone(),
         }
     }
 }
@@ -45,7 +55,7 @@ impl Widget for PipeWidget {
             }
         };
 
-        pipe_config
+        let content = pipe_config
             .format
             .iter()
             .map(|f| {
@@ -61,11 +71,29 @@ impl Widget for PipeWidget {
                 (f, content)
             })
             .fold("".to_owned(), |acc, (f, content)| {
-                format!("{acc}{}", f.format_string(&content))
-            })
+                if pipe_config.render_mode == RenderMode::Static {
+                    return format!("{acc}{}", f.format_string(&content));
+                }
+
+                format!("{acc}{}", content)
+            });
+
+        match pipe_config.render_mode {
+            RenderMode::Static => content,
+            RenderMode::Dynamic => render_dynamic_formatted_content(&content, &self.zj_conf),
+            RenderMode::Raw => pipe_result.to_owned(),
+        }
     }
 
     fn process_click(&self, _name: &str, _state: &crate::config::ZellijState, _pos: usize) {}
+}
+
+fn render_dynamic_formatted_content(content: &str, config: &BTreeMap<String, String>) -> String {
+    formatted_parts_from_string_cached(content, config)
+        .iter()
+        .map(|fp| fp.format_string(&fp.content))
+        .collect::<Vec<String>>()
+        .join("")
 }
 
 fn parse_config(zj_conf: &BTreeMap<String, String>) -> BTreeMap<String, PipeConfig> {
@@ -80,7 +108,10 @@ fn parse_config(zj_conf: &BTreeMap<String, String>) -> BTreeMap<String, PipeConf
 
     for key in keys {
         let pipe_name = PIPE_REGEX.replace(&key, "").to_string();
-        let mut pipe_conf = PipeConfig { format: vec![] };
+        let mut pipe_conf = PipeConfig {
+            format: vec![],
+            render_mode: RenderMode::Static,
+        };
 
         if let Some(existing_conf) = config.get(pipe_name.as_str()) {
             pipe_conf = existing_conf.clone();
@@ -89,6 +120,18 @@ fn parse_config(zj_conf: &BTreeMap<String, String>) -> BTreeMap<String, PipeConf
         if key.ends_with("format") {
             pipe_conf.format =
                 FormattedPart::multiple_from_format_string(zj_conf.get(&key).unwrap(), zj_conf);
+        }
+
+        if key.ends_with("rendermode") {
+            pipe_conf.render_mode = match zj_conf.get(&key) {
+                Some(mode) => match mode.as_str() {
+                    "static" => RenderMode::Static,
+                    "dynamic" => RenderMode::Dynamic,
+                    "raw" => RenderMode::Raw,
+                    _ => RenderMode::Static,
+                },
+                None => RenderMode::Static,
+            };
         }
 
         config.insert(pipe_name, pipe_conf);
